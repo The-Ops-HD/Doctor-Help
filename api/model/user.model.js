560cb1185b699508423c508593111f019fd6f47d
Verified ✔️ <ryesan58@gmail.com>
2023-02-05 23:14:33 -0500

    Update user.model.js

--- api/model/user.model.js
+++ api/model/user.model.js
@@ -3,58 +3,20 @@ const bcrypt = require('bcrypt');
 
 
 
-const USerSchema = new mongoose.Schema({
-	FirstName: {
+const PatientSchema = new mongoose.Schema({
+	PatientId: {
 	type: String,
-	required: [true, 'Must have a Firstname'],
-	minLength: [3, 'Must be more than 2 characters!'],
+	required: [true],
+	minLength: [3],
 },
-	LastName: {
+	Description: {
 		type: String,
-		required: [true, "Must have a Last Name" ],
-    minLength: [3, 'Must be more than 2 characters!']
+		required: [true],
+    minLength: [3],
 	},
-  email: {
-    type: String,
-    required: [true, 'Must Add an Email'],
-    validate: {
-      validator: (val) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(val),
-      message: "Please Enter Valid email"
-    },
-  },
-  password: {
-    type: String,
-    required: [true, "Password is Required"],
-    minlength: [8, "Password must be 8 characters or longer"]
-  }
 }, {timestamps : true})
 
-USerSchema.virtual('confirmPassword')
-  .get(() => this.confirmPassword)
-  .set((value) => {
-    this.confirmPassword = value;
-  });
 
 
-USerSchema.pre(('validate'), function (next) {
-  if (this.password !== this.confirmPassword){
-    this.invalidate("confirmPassword", "Passwords must match");
-  }
-  next();
-});
 
-USerSchema.pre('save', function (next){
-  bcrypt
-    .hash(this.password, 10)
-    .then((hash) => {
-      this.password = hash; 
-      next();
-    })
-    .catch((err) => {
-      console.log("error saving hash")
-      console.log(err)
-    })
-  });
-
-
-module.exports = mongoose.model("User", USerSchema);
+module.exports = mongoose.model("Patient", PatientSchema);

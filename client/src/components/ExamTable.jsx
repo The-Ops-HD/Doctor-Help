082fef3c6b9a26b3bde8590bce316cee86f69810
AP <74792335+aaronpagoada@users.noreply.github.com>
2023-02-06 14:52:46 -0500

    Edited ExamTable styling

--- client/src/components/ExamTable.jsx
+++ client/src/components/ExamTable.jsx
@@ -2,32 +2,29 @@ import React from "react";
 import ExamTableRow from "./ExamTableRow";
 
 function ExamTable(){
-
-
-    return(
-        <div id="examTableContainer">
-             <table className='examTable'>
-                <thead id="examTableHeader">
-                    <tr>
-                        <th>Patient ID</th>
-                        <th>Age</th>
-                        <th>Sex</th>
-                        <th>Zip Code</th>
-                        <th>BMI</th>
-                        <th>Exam ID</th>
-                        <th>Key Findings</th>
-                        <th> Brixia Scores</th>
-                        <th>Image</th>
-                    </tr>
-                </thead>
-                <tbody id="examTableBody">
-                    <ExamTableRow/>
-                    <ExamTableRow/>
-                </tbody>
-            </table>
-        </div>
-       
-    )
+  return(
+    <div id="examTableContainer">
+      <table className='examTable'>
+        <thead id="examTableHeader">
+          <tr>
+            <th>Patient ID</th>
+            <th>Age</th>
+            <th>Sex</th>
+            <th>ZIP Code</th>
+            <th>BMI</th>
+            <th>Exam ID</th>
+            <th>Key Findings</th>
+            <th>Brixia Scores</th>
+            <th>Image</th>
+          </tr>
+        </thead>
+        <tbody id="examTableBody">
+          <ExamTableRow/>
+          <ExamTableRow/>
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
 export default ExamTable;

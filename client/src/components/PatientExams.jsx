271228b88ff77f506ebace64446fb8282a54229d
Verified ✔️ <ryesan58@gmail.com>
2023-02-28 05:09:31 -0500

    Update PatientExams.jsx

--- client/src/components/PatientExams.jsx
+++ client/src/components/PatientExams.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import ExamTable from "./ExamTable";
 import axios from 'axios';
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import PatientButton from "./PatientButton";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 const PatientExams = (props) => {
   const {PatientId} = useParams();
@@ -22,51 +28,51 @@ const PatientExams = (props) => {
   }, []);
 
   return(
-    <div id="examTableContainer">
-      <p>All Examinations from {PatientId}</p>
-      <table className='examTable'>
-        <thead id="examTableHeader">
-          <tr>
-            <th>Patient ID</th>
-            <th>Age</th>
-            <th>Sex</th>
-            <th>ZIP Code</th>
-            <th>BMI</th>
-            <th>Exam ID</th>
-            <th>Key Findings</th>
-            <th>Brixia Scores</th>
-            <th>Image</th>
-            <th>Expand Details</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* data goes here */}
-          {patients.map((patient, index) => {
-        return(
-            <tr key={index}>
-              <td>
-                <Link to = {`http://localhost:3000/patientexam/${patient.PatientId}`}>
-                  {patient.PatientId}
-                </Link>
-              </td>
-              <td>{patient.Age}</td>
-              <td>{patient.Sex}</td>
-              <td>{patient.ZipCode}</td>
-              <td>{patient.bmi}</td>
-              <td>{patient.ExamID}</td>
-              <td>{patient.KeyFindings}</td>
-              <td>{patient.BrixiaScore}</td>
-              <td>
-                <img src={patient.ImageUrl} alt="x-ray" style={{height: '100px', width: '100px'}}/>
-              </td>
-              <td>
-              <PatientButton id = {patient._id}/>
-              </td>
-            </tr>
-        )
-      })}
-        </tbody>
-      </table>
+    <div className="mui-table">
+      <TableContainer sx={{ maxWidth: 1100 }} component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Patient ID</TableCell>
+              <TableCell align="right">Exam ID</TableCell>
+              <TableCell align="right">Image</TableCell>
+              <TableCell align="right">Key Findings</TableCell>
+              <TableCell align="right">Brixia Score</TableCell>
+              <TableCell align="right">Age</TableCell>
+              <TableCell align="right">Sex</TableCell>
+              <TableCell align="right">BMI</TableCell>
+              <TableCell align="right">ZIP Code</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {patients.map((patient, index) => {
+              return (
+                <TableRow key={index}>
+                  <TableCell component="th" scope="row">
+                    <Link to={`http://localhost:3000/patientexam/${patient.PatientId}`}>
+                      {patient.PatientId}
+                    </Link>
+                  </TableCell>
+                  <TableCell align="right">
+                    <Link to={`http://localhost:3000/details/${patient._id}`}>
+                      {patient.ExamID}
+                    </Link>
+                  </TableCell>
+                  <TableCell>
+                    <img src={patient.ImageURL} alt="x-ray" style={{height: '50px', width: '50px'}}/>
+                  </TableCell>
+                  <TableCell align="right">{patient.KeyFindings}</TableCell>
+                  <TableCell align="right">{patient.BrixiaScore}</TableCell>
+                  <TableCell align="right">{patient.Age}</TableCell>
+                  <TableCell align="right">{patient.Sex}</TableCell>
+                  <TableCell align="right">{patient.bmi}</TableCell>
+                  <TableCell align="right">{patient.ZipCode}</TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   )
 }

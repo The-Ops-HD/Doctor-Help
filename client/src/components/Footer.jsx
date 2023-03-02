60c13db6ea491d4a0d758004e53945dd3a555f82
AP <74792335+aaronpagoada@users.noreply.github.com>
2023-03-02 01:19:57 -0500

    Added footer styling

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import "../component-css/Footer.css"
 
 const Footer = () => {
   return (
-    <div>
-      <h1>FOOTER</h1>
+    <div className="footer">
+      <p>
+        © 2023 The Ops, powered by 
+        <a href="https://www.hackdiversity.com/"> Hack.Diversity</a>  
+      </p>
     </div>
   )
 }

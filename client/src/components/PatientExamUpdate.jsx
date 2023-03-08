6f10ce225561af28fcff959752e25424c1d612b2
AP <74792335+aaronpagoada@users.noreply.github.com>
2023-03-08 00:46:20 -0500

    Edited ExamUpdate styling

--- client/src/components/PatientExamUpdate.jsx
+++ client/src/components/PatientExamUpdate.jsx
@@ -6,7 +6,7 @@ import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Button } from "@mui/material";
-
+import '../component-css/PatientExamUpdate.css'
 
 // create a page that allows the user to edit the information
 //pass in that information that is used on the view page
@@ -15,76 +15,158 @@ import { Button } from "@mui/material";
 //make this a form
 
 const UpdateForms = ({details, setDetails}) => {
-    const navigate = useNavigate();
-    const {id} = useParams();
+  const navigate = useNavigate();
+  const {id} = useParams();
 
+  function handleChange(event){
+    setDetails({
+      ...details,
+      [event.target.name]: event.target.value,
+    })
+  }
+  
+  const updateHandler = (e) => {
+    e.preventDefault();
+    axios.put(`http://localhost:9000/api/edit/${id}`, {details})
+      .then((res) => {
+        console.log('SUCCESS', res);
+        navigate(`/details/${id}`);
+      })
+      .catch((err) => {
+        console.log('ERROR!', err.response);
+      })
+  }
 
-    function handleChange(event){
-        setDetails({
-            ...details,
-            [event.target.name] : event.target.value,
-        })
-    }
-   
-
-     
-    const updateHandler = (e) => {
-        e.preventDefault();
-        axios.put(`http://localhost:9000/api/edit/${id}`, {
-         details
-        })
-            .then((res) => {
-                console.log('SUCCESS', res);
-                navigate(`/details/${id}`);
-            })
-            .catch((err) => {
-                console.log('ERROR!', err.response);
-            })
-    }
-
-    return (
-        <div className="patient-exam-container">
-            <form onSubmit={updateHandler} style={{ width: "82%", marginLeft: "11%" }}>
-                <Box pl={10} sx={{ width: '100%' }}>
-                    <div style={{ textAlign: "center", marginLeft: "-210px", marginTop: "15px" }}>
-                        <Button type="submit" sx={{ backgroundColor: '#b6bf88', ':hover': { bgcolor: '#578188', color:'white'}  }} variant="contained">Update</Button>
-                    </div>
-                    <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                        <Grid item xs={6}>
-                            <TextField sx={{ m: 1, width: '35ch' }} id="outlined-basic" label="Patient ID" variant="outlined" onChange={handleChange} type="text" value={details.PatientId} name={"PatientId"}></TextField>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <TextField sx={{ m: 1, width: '35ch' }} id="outlined-basic" label="EXAM ID" variant="outlined" onChange={handleChange} type="text" value={details.ExamID} name={"ExamID"}></TextField>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <TextField sx={{ m: 1, width: '35ch' }} id="outlined-basic" label="Age" variant="outlined" onChange={handleChange} type="text" value={details.Age} name={"Age"}></TextField>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <TextField sx={{ m: 1, width: '35ch' }} id="outlined-basic" label="Image URL" variant="outlined" onChange={handleChange} type="text" value={details.ImageUrl} name={"ImageUrl"}></TextField>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <TextField sx={{ m: 1, width: '35ch' }} id="outlined-basic" label="Sex" variant="outlined" onChange={handleChange} type="text" value={details.Sex} name={"Sex"}></TextField>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <TextField sx={{ m: 1, width: '35ch' }} id="outlined-basic" label="Date" variant="outlined" onChange={handleChange} type="text" value={details.Date} name={"Date"}></TextField>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <TextField sx={{ m: 1, width: '35ch' }} id="outlined-basic" label="BMI" variant="outlined" onChange={handleChange} type="text" value={details.bmi} name={"bmi"}></TextField>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <TextField sx={{ m: 1, width: '35ch' }} id="outlined-basic" label="Key Findings" variant="outlined" onChange={handleChange} type="text" value={details.KeyFindings} name={"KeyFindings"}></TextField>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <TextField sx={{ m: 1, width: '35ch' }} id="outlined-basic" label="Zip Code" variant="outlined" onChange={handleChange} type="text" value={details.ZipCode} name={"ZipCode"}></TextField>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <TextField sx={{ m: 1, width: '35ch' }} id="outlined-basic" label="Brixia Score" variant="outlined" onChange={handleChange} type="text" value={details.BrixiaScore} name={"BrixiaScore"}></TextField>
-                        </Grid>
-                    </Grid>
-                </Box>
-            </form>
+  return (
+    <div className="update-form-container">
+      <div className="update-button">
+        <Button
+          type="submit"
+          sx={{
+            textTransform: 'none',
+            backgroundColor: '#578188' ,
+            ':hover': { 
+              bgcolor: '#3b5559', 
+              color:'white'
+            } 
+          }} 
+          variant="contained"
+        >
+          Update
+        </Button>
+      </div>
+      <form 
+        className="update-form"
+        onSubmit={updateHandler} 
+        style={{ width: "75%" }}
+      >
+        <div className="form-cols">
+          <TextField 
+            sx={{ m: 1, width: '35ch' }} 
+            id="outlined-basic" 
+            label="Patient ID" 
+            variant="outlined" 
+            onChange={handleChange} 
+            type="text"
+            value={details.PatientId} 
+            name={"PatientId"}
+          ></TextField>
+          <TextField 
+            sx={{ m: 1, width: '35ch' }}  
+            id="outlined-basic" 
+            label="Age" 
+            variant="outlined" 
+            onChange={handleChange} 
+            type="text"
+            value={details.Age} 
+            name={"Age"}
+          ></TextField>
+          <TextField 
+            sx={{ m: 1, width: '35ch' }}  
+            id="outlined-basic" 
+            label="Sex" 
+            variant="outlined" 
+            onChange={handleChange}
+            type="text"
+            value={details.Sex}
+            name={"Sex"}
+          ></TextField>
+          <TextField 
+            sx={{ m: 1, width: '35ch' }}  
+            id="outlined-basic" 
+            label="BMI" 
+            variant="outlined" 
+            onChange={handleChange}
+            type="text"
+            value={details.bmi} 
+            name={"bmi"}
+          ></TextField>
+          <TextField 
+            sx={{ m: 1, width: '35ch' }}  
+            id="outlined-basic" 
+            label="ZIP Code" 
+            variant="outlined" 
+            onChange={handleChange}
+            type="text"
+            value={details.ZipCode} 
+            name={"ZipCode"}
+          ></TextField>
         </div>
-    )
+        <div className="form-cols">
+          <TextField 
+            sx={{ m: 1, width: '35ch' }} 
+            id="outlined-basic" 
+            label="Exam ID" 
+            variant="outlined" 
+            onChange={handleChange} 
+            type="text"
+            value={details.ExamID} 
+            name={"ExamID"}
+          ></TextField>
+          <TextField 
+            sx={{ m: 1, width: '35ch' }}  
+            id="outlined-basic" 
+            label="Image URL" 
+            variant="outlined" 
+            onChange={handleChange}
+            type="text"
+            value={details.ImageUrl} 
+            name={"ImageUrl"}
+          ></TextField>
+          <TextField 
+            sx={{ m: 1, width: '35ch' }}  
+            id="outlined-basic" 
+            label="Date" 
+            variant="outlined" 
+            onChange={handleChange}
+            type="text"
+            value={details.Date} 
+            name={"Date"}
+          ></TextField>
+          <TextField  
+            sx={{ m: 1, width: '35ch' }} 
+            id="outlined-basic" 
+            label="Key Findings" 
+            variant="outlined" 
+            onChange={handleChange}
+            type="text"
+            value={details.KeyFindings} 
+            name={"KeyFindings"}
+          ></TextField>
+          <TextField 
+            sx={{ m: 1, width: '35ch' }}  
+            id="outlined-basic" 
+            label="Brixia Score" 
+            variant="outlined" 
+            onChange={handleChange}
+            type="text"
+            value={details.BrixiaScore} 
+            name={"BrixiaScore"}
+          ></TextField>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default UpdateForms
